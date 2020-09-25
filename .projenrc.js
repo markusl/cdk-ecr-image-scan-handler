a8b41c4c5fe126a17f45c33da82639f087535a9b
Markus Lindqvist <markus.lindqvist@op.fi>
2020-09-25 13:08:38 +0300

    Upgrade CDK

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,7 @@ const {
   Semver
 } = require('projen');
 
-const AWS_CDK_LATEST_RELEASE = '1.63.0';
+const AWS_CDK_LATEST_RELEASE = '1.64.0';
 const CONSTRUCTS_LATEST_RELEASE = '3.0.4';
 const PROJECT_NAME = 'cdk-ecr-image-scan-handler';
 const PROJECT_DESCRIPTION = 'A JSII construct lib for ECR image scan results handling';
@@ -17,29 +17,29 @@ const project = new JsiiProject({
   authorEmail: 'markus.lindqvist@iki.fi',
   stability: 'experimental',
   devDependencies: {
-    '@aws-cdk/assert': Semver.pinned(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/assert': Semver.caret(AWS_CDK_LATEST_RELEASE),
     '@types/jest': Semver.caret('26.0.10'),
     '@types/node': Semver.caret('14.6.0'),
     'parcel': Semver.pinned('2.0.0-beta.1'),
   },
   dependencies: {
     constructs: Semver.pinned(CONSTRUCTS_LATEST_RELEASE),
-    '@aws-cdk/core': Semver.pinned(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-lambda': Semver.pinned(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-lambda-nodejs': Semver.pinned(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-iam': Semver.pinned(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-sns': Semver.pinned(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-sns-subscriptions': Semver.pinned(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/core': Semver.caret(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/aws-lambda': Semver.caret(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/aws-lambda-nodejs': Semver.caret(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/aws-iam': Semver.caret(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/aws-sns': Semver.caret(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/aws-sns-subscriptions': Semver.caret(AWS_CDK_LATEST_RELEASE),
     'aws-sdk': Semver.caret('2.751.0'),
   },
   peerDependencies: {
     constructs: Semver.pinned(CONSTRUCTS_LATEST_RELEASE),
-    '@aws-cdk/core': Semver.pinned(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-lambda': Semver.pinned(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-lambda-nodejs': Semver.pinned(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-iam': Semver.pinned(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-sns': Semver.pinned(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-sns-subscriptions': Semver.pinned(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/core': Semver.caret(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/aws-lambda': Semver.caret(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/aws-lambda-nodejs': Semver.caret(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/aws-iam': Semver.caret(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/aws-sns': Semver.caret(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/aws-sns-subscriptions': Semver.caret(AWS_CDK_LATEST_RELEASE),
   },
   bundledDependencies: [
     'aws-sdk',
