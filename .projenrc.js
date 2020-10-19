4d18eabf5ee247a99da4cc398c650c2a2df4ea26
Markus Lindqvist <markus.lindqvist@op.fi>
2020-10-19 14:57:20 +0300

    Use 'AwsCdkConstructLibrary' template for generating the project

--- .projenrc.js
+++ .projenrc.js
@@ -1,21 +1,29 @@
 const {
-  JsiiProject,
-  Semver
+  AwsCdkConstructLibrary,
+  Semver,
 } = require('projen');
 
 const AWS_CDK_LATEST_RELEASE = '1.68.0';
-const CONSTRUCTS_LATEST_RELEASE = '3.0.4';
+
 const PROJECT_NAME = 'cdk-ecr-image-scan-handler';
 const PROJECT_DESCRIPTION = 'A JSII construct for ECR image scan results processing and reporting via Email or Microsoft Teams webhook';
 
-const project = new JsiiProject({
+const project = new AwsCdkConstructLibrary({
   name: PROJECT_NAME,
-  jsiiVersion: Semver.caret('1.13.0'),
   description: PROJECT_DESCRIPTION,
   repository: 'https://github.com/markusl/cdk-ecr-image-scan-handler.git',
   authorName: 'Markus Lindqvist',
   authorEmail: 'markus.lindqvist@iki.fi',
   stability: 'stable',
+  cdkVersion: AWS_CDK_LATEST_RELEASE,
+  cdkDependencies: [
+    '@aws-cdk/core',
+    '@aws-cdk/aws-lambda',
+    '@aws-cdk/aws-lambda-nodejs',
+    '@aws-cdk/aws-iam',
+    '@aws-cdk/aws-sns',
+    '@aws-cdk/aws-sns-subscriptions',
+  ],
   devDependencies: {
     '@aws-cdk/assert': Semver.caret(AWS_CDK_LATEST_RELEASE),
     '@types/jest': Semver.caret('26.0.14'),
@@ -24,27 +32,13 @@ const project = new JsiiProject({
     'parcel': Semver.pinned('2.0.0-beta.1'),
   },
   dependencies: {
-    constructs: Semver.pinned(CONSTRUCTS_LATEST_RELEASE),
-    '@aws-cdk/core': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-lambda': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-lambda-nodejs': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-iam': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-sns': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-sns-subscriptions': Semver.caret(AWS_CDK_LATEST_RELEASE),
     'aws-sdk': Semver.caret('2.751.0'),
+    'aws-lambda': Semver.caret('1.0.6'),
     'node-fetch': Semver.caret('2.6.1')
-  },
-  peerDependencies: {
-    constructs: Semver.pinned(CONSTRUCTS_LATEST_RELEASE),
-    '@aws-cdk/core': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-lambda': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-lambda-nodejs': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-iam': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-sns': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-sns-subscriptions': Semver.caret(AWS_CDK_LATEST_RELEASE),
   },
   bundledDependencies: [
     'aws-sdk',
+    'aws-lambda',
     'node-fetch',
   ]
 });
